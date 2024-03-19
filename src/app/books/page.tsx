e50d25d9944c10efa64aeafda466d563ee816dd5
george4joseph <geojos.rit@gmail.com>
2024-03-19 15:44:13 +0530

    n

---
+++ src/app/books/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Books() {
+  return(
+    <div>books page</div>
+  )
+}
---
+++ src/app/members/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Members() {
+  return(
+    <div>Members page</div>
+  )
+}
